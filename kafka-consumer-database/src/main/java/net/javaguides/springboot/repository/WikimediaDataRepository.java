12f15afdcfcfe72f7ff93b2bb207c686ff193595
wulitina <tinamazing.yan.xu@gmail.com>
2024-08-16 14:18:54 +0100

    configure mysql database & save wikimedia data into mysql database

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,8 @@
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
